16ea83adf9074e56b62af17228be316e73cebd95
Nick Jennings <nick@silverbucket.net>
2014-09-11 19:35:22 +0200

    refactored a bit to allow for irc chatroom messages to be included in filter

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -7,25 +7,31 @@ function ($scope, $route, $document, $location, messages, accounts, contacts, co
   function inThisConversation (m) {
     if (!$scope.contact) { return false; }
     // targets list of IDs
-    var tmp = contact.email.concat(contact.impp);
+    var tmp = contact.email || [];
+    tmp = tmp.concat(contact.impp);
     var chatIDs = [];
     for (var i = tmp.length -1; i >= 0; i--) {
       chatIDs.push(tmp[i].value);
     }
-console.log('comparing chatIds: ', chatIDs);
-console.log('comparing m: ', m);
 
-    if ((chatIDs.indexOf(m.actor.address) > -1) ||
-        ((chatIDs.indexOf(m.target.address) > -1) &&
-         (accounts.indexOf(m.actor.address) > -1))) {
+    if (chatIDs.indexOf(m.actor.address) > -1) {
       return true;
+    } else if (typeof m.target.length === 'number') {
+      //if (accounts.indexOf(m.actor.address) > -1) {
+        for (i = m.target.length -1; i >= 0; i--) {
+          if (chatIDs.indexOf(m.target[i].address) > -1) {
+            return true;
+          } else if (m.target[i].address === contact.fn) {
+            return true;
+          }
+        }
+      //}
     } else {
       return false;
     }
   }
 
   function mergeMessages() {
-    console.log('mergeMessages() called');
     var mergedMessages = [];
     for (var key in messages) {
       for (var mk in messages[key]) {
@@ -41,9 +47,7 @@ console.log('comparing m: ', m);
   $scope.contacts = contacts;
   $scope.accounts = accounts;
   $scope.messages = messages;
-  //$scope.mergedMessages = mergeMessages();
-  //console.log('--MSGS: ', $scope.mergedMessages);
-  console.log('CONTACT: ', contact);
+  $scope.mergedMessages = mergeMessages();
 
   $scope.data = {
     selected: undefined,
@@ -60,7 +64,7 @@ console.log('comparing m: ', m);
   };
 
   $scope.$watchCollection('messages', function (oldValue, newValue) {
-    console.log('FIRED: ', oldValue, newValue);
+    //console.log('FIRED: ', oldValue, newValue);
     $scope.mergedMessages = mergeMessages(messages);
   });
 
